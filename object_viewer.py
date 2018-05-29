b16d900fde16e9b61604b3f0aa83f002dcdc8459
Chris <Lohrey.Christian@gmail.com>
2018-05-29 20:40:33 +0200

    First

---
+++ object_viewer.py
@@ -0,0 +1,10 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from OpenGL.arrays import vbo
+
+import numpy as np
+import sys
+import math
+from Fileparser import read_obj
+
